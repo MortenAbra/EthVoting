d9db6806fdcaa0ad26c9b9309b1a5507a68191af
MortenAbra <sezure@gmail.com>
2019-05-30 14:03:47 +0200

    -Improved ballot

--- contracts/Ballot.sol
+++ contracts/Ballot.sol
@@ -9,14 +9,25 @@ contract Ballot {
         uint candidateVotes;
     }
 
+    struct Voter {
+        bool authorized;
+        bool voted;
+        uint vote;
+    }
+
     //Mapping of both candidates and voters
     //Both stores each representive key type and value type
     mapping(uint => Candidate) public candidates;
-    mapping(address => bool) public voters;
+    mapping(address => Voter) public voters;
 
     //Amount of candidates
     uint public totalCandidates;
+    address public owner;
 
+    modifier ownerOnly(){
+        require(msg.sender == owner);
+        _;
+    }
 
     //Function to add new candidates
     function newCandidate(string _name) private {
@@ -24,15 +35,17 @@ contract Ballot {
         candidates[totalCandidates] = Candidate(totalCandidates, _name, 0);
     }
 
+    function authorize(address _person) ownerOnly public {
+        voters[_person].authorized = true;
+    }
+
     //Function for voting on a candidate
     function candidateVote (uint _id) public {
         //Requires a voter not to have already voted
-        require(!voters[msg.sender]);
+        require(!voters[msg.sender].voted);
+        require(voters[msg.sender].authorized);
         //Needs a valid candidate for the voter to vote
         require(_id > 0 && _id <= totalCandidates);
-
-        //Voter has now voted
-        voters[msg.sender] = true;
 
         //Updates the number of votes for the candidate voted for
         candidates[_id].candidateVotes++;
@@ -45,9 +58,14 @@ contract Ballot {
 
     //Constructor - Initialize new candidates to vote for
     constructor () public {
+        owner = msg.sender;
         newCandidate("Morten");
         newCandidate("Mads");
         newCandidate("Dennis");
+    }
+
+    function endElection() ownerOnly public {
+        selfdestruct(owner);
     }
 
 }
